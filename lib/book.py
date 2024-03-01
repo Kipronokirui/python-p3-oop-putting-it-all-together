8d66730d023059c2d7b33612dbf3a6701dfdb61c
Kipronokirui <victorkipronokirui2@gmail.com>
2024-03-01 17:43:19 +0300

    Lab Done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count): 
+        self.title = title 
+        self.page_count = page_count 
+    
+    def show_book_title(self, newTitle):
+        self.title =  newTitle  
+        print('The book title is:', self.title)
+
+    def show_page_count(self):   
+        print('The book page count is:', self.page_count)
+
+title = Book("Manchester United", 23)
+title.show_book_title("Chelsea")
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand, size): 
+        self.brand = brand 
+        self.size = size 
     pass
