aee7d9d4fa2f92c5f7521699896391004cefaced
Kipronokirui <victorkipronokirui2@gmail.com>
2024-03-23 20:24:34 +0300

    Tests Passed

--- lib/book.py
+++ lib/book.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 
 class Book:
-    def __init__(self, title, page_count): 
-        self.title = title 
-        self.page_count = page_count 
+    def __init__(self, title, page_count):
+        self._title = title
+        self._page_count = page_count
     
-    def show_book_title(self, newTitle):
-        self.title =  newTitle  
-        print('The book title is:', self.title)
-
-    def show_page_count(self):   
-        print('The book page count is:', self.page_count)
-
-title = Book("Manchester United", 23)
-title.show_book_title("Chelsea")
-    
+    @property
+    def title(self):
+        """The title property"""
+        return self._title
         
+    @title.setter
+    def title(self, title):
+        pass
+
+    @property
+    def page_count(self):
+        """Page count property"""
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, page_count):
+        """Page count must be an integer"""
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print('page_count must be an integer')
+    
+    def turn_page(self):
+        print('Flipping the page...wow, you read fast!')
